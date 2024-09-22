f2ad0d8482633d2c332a5a4e9635a8e43372fd4f
julzybae <juliogerald25@gmail.com>
2024-09-22 13:27:00 +0700

    feat: nav-link active

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,51 +5,54 @@
 
         @auth('admin')
             <li class="nav-item">
-                <a class="nav-link collapsed active" href="{{ route('admin.dashboard') }}">
+                <a class="nav-link {{ request()->routeIs('admin.dashboard') ? 'active' : '' }}"
+                    href="{{ route('admin.dashboard') }}">
                     <i class="bi bi-speedometer2"></i>
                     <span>Dashboard</span>
                 </a>
             </li><!-- End Dashboard Nav -->
 
             <li class="nav-item">
-                <a class="nav-link collapsed" href="{{ route('admin.users') }}">
+                <a class="nav-link {{ request()->routeIs('admin.users') ? 'active' : '' }}"
+                    href="{{ route('admin.users') }}">
                     <i class="bi bi-people"></i>
                     <span>Pengguna</span>
                 </a>
             </li><!-- End Users Nav -->
 
             <li class="nav-item">
-                <a class="nav-link collapsed" href="index.html">
+                <a class="nav-link" href="index.html">
                     <i class="bi bi-grid"></i>
                     <span>Kategori</span>
                 </a>
             </li><!-- End Categories Nav -->
 
             <li class="nav-item">
-                <a class="nav-link collapsed" href="index.html">
+                <a class="nav-link" href="index.html">
                     <i class="bi bi-building"></i>
-                    <span>Kelurahan</span>
+                    <span>Kalurahan</span>
                 </a>
-            </li><!-- End Categories Nav -->
+            </li><!-- End Sub Kalurahan Nav -->
         @endauth
 
         @auth('user')
             <li class="nav-item">
-                <a class="nav-link active" href="{{ route('user.dashboard') }}">
+                <a class="nav-link {{ request()->routeIs('user.dashboard') ? 'active' : '' }}"
+                    href="{{ route('user.dashboard') }}">
                     <i class="bi bi-speedometer2"></i>
                     <span>Dashboard</span>
                 </a>
             </li><!-- End Dashboard Nav -->
 
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#">
+                <a class="nav-link" href="#">
                     <i class="bi bi-bag"></i>
                     <span>Produk</span>
                 </a>
             </li><!-- End Produk Nav -->
 
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#">
+                <a class="nav-link" href="#">
                     <i class="bi bi-badge-ad"></i>
                     <span>Iklan</span>
                 </a>
